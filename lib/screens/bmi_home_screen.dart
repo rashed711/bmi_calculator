9ad7bfbd8eadb42f27263171f0d7d58f887d18f9
Rashed <rashed1711@gmail.com>
2025-03-23 17:35:06 +0200

    Ahmed Rashed

--- lib/screens/bmi_home_screen.dart
+++ lib/screens/bmi_home_screen.dart
@@ -18,175 +18,173 @@ class _BmiHomeScreenState extends State<BmiHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('BMI Calculator'), centerTitle: true),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: buildGanderItem(
-                        icon: Icons.male,
-                        isActive: isMale,
-                        text: 'MALE',
-                        onPressed: () {
-                          setState(() {
-                            isMale = true;
-                          });
-                        },
-                      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('BMI Calculator'), centerTitle: true),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: buildGanderItem(
+                      icon: Icons.male,
+                      isActive: isMale,
+                      text: 'MALE',
+                      onPressed: () {
+                        setState(() {
+                          isMale = true;
+                        });
+                      },
                     ),
-
-                    const SizedBox(width: 10),
-
-                    Expanded(
-                      child: buildGanderItem(
-                        icon: Icons.female,
-                        isActive: !isMale,
-                        text: 'FEMAL',
-                        onPressed: () {
-                          setState(() {
-                            isMale = false;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(12),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        'HIGHT',
-                        style: TextStyle(fontSize: 25, color: Colors.white),
-                      ),
 
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            height.toString(),
-                            style: TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Text(
-                            'CM',
-                            style: TextStyle(fontSize: 15, color: Colors.white),
-                          ),
-                        ],
-                      ),
+                  const SizedBox(width: 10),
 
-                      Slider(
-                        min: 120,
-                        max: 200,
-                        value: height.toDouble(),
-                        thumbColor: Colors.red,
-                        activeColor: Colors.white,
-                        inactiveColor: Colors.red,
-                        onChanged: (value) {
-                          setState(() {
-                            height = value.toInt();
-                          });
-                        },
-                      ),
-                    ],
+                  Expanded(
+                    child: buildGanderItem(
+                      icon: Icons.female,
+                      isActive: !isMale,
+                      text: 'FEMAL',
+                      onPressed: () {
+                        setState(() {
+                          isMale = false;
+                        });
+                      },
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
+          ),
 
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: buildWeightOrAgeContainer(
-                        titel: "WEIGHT",
-                        value: weight,
-                        addfunciton: () {
-                          setState(() {
-                            weight++;
-                          });
-                        },
-                        removefunction: () {
-                          setState(() {
-                            weight--;
-                          });
-                        },
-                      ),
-                    ),
-
-                    const SizedBox(width: 10),
-
-                    Expanded(
-                      child: buildWeightOrAgeContainer(
-                        titel: "AGE",
-                        value: age,
-                        addfunciton: () {
-                          setState(() {
-                            age++;
-                          });
-                        },
-                        removefunction: () {
-                          setState(() {
-                            age--;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  double result = weight / ((height / 100) * (height / 100));
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => ResultScreen(result: result),
-                    ),
-                  );
-                },
-                child: Container(
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
                   color: Colors.black,
-                  child: const Center(
-                    child: Text(
-                      'Calculate BMI',
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'HIGHT',
                       style: TextStyle(fontSize: 25, color: Colors.white),
                     ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          height.toString(),
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'CM',
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                      ],
+                    ),
+
+                    Slider(
+                      min: 120,
+                      max: 200,
+                      value: height.toDouble(),
+                      thumbColor: Colors.red,
+                      activeColor: Colors.white,
+                      inactiveColor: Colors.red,
+                      onChanged: (value) {
+                        setState(() {
+                          height = value.toInt();
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: buildWeightOrAgeContainer(
+                      titel: "WEIGHT",
+                      value: weight,
+                      addfunciton: () {
+                        setState(() {
+                          weight++;
+                        });
+                      },
+                      removefunction: () {
+                        setState(() {
+                          weight--;
+                        });
+                      },
+                    ),
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  Expanded(
+                    child: buildWeightOrAgeContainer(
+                      titel: "AGE",
+                      value: age,
+                      addfunciton: () {
+                        setState(() {
+                          age++;
+                        });
+                      },
+                      removefunction: () {
+                        setState(() {
+                          age--;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                double result = weight / ((height / 100) * (height / 100));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ResultScreen(result: result),
+                  ),
+                );
+              },
+              child: Container(
+                color: Colors.black,
+                child: const Center(
+                  child: Text(
+                    'Calculate BMI',
+                    style: TextStyle(fontSize: 25, color: Colors.white),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
